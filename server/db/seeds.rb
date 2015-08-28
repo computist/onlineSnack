e9ba5d2d96fa626e1fca4d4283008a9289fb4044
Yingkai Wang <yingkaiw@zillow.com>
2015-08-27 21:51:49 -0700

    change chinese to utf

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -14,10 +14,10 @@
 # 	end
 # end
 # shell.say "Bye"
-Dish.create(:name => "è¾£å­é¸¡", :location => "å››å·", :spicy => 5, :rate => 4.1, :rate_number => 12)
-Dish.create(:name => "æ¯›è¡€æ—º", :location => "é‡åº†", :spicy => 4, :rate => 3.5, :rate_number => 7)
-Dish.create(:name => "ç«é”…", :location => "å››å·", :spicy => 5, :rate => 4.3, :rate_number => 20)
-Dish.create(:name => "åŒ—äº¬çƒ¤é¸­", :location => "åŒ—äº¬", :spicy => 0, :rate => 3.4, :rate_number =>16)
-Dish.create(:name => "çƒ¤å…¨ç¾Š", :location => "æ–°ç–†", :spicy => 2, :rate => 4.3, :rate_number => 13)
-Dish.create(:name => "è‚ ç²‰", :location => "å¹¿å·ž", :spicy => 0, :rate => 3.2, :rate_number => 17)
-Dish.create(:name => "çº¢çƒ§æ­¦æ˜Œé±¼", :location => "æ¹–åŒ—", :spicy => 3, :rate => 3.9, :rate_number => 9)
+Dish.create(:name => "À±×Ó¼¦", :location => "ËÄ´¨", :spicy => 5, :rate => 4.1, :rate_number => 12)
+Dish.create(:name => "Ã«ÑªÍú", :location => "ÖØÇì", :spicy => 4, :rate => 3.5, :rate_number => 7)
+Dish.create(:name => "»ð¹ø", :location => "ËÄ´¨", :spicy => 5, :rate => 4.3, :rate_number => 20)
+Dish.create(:name => "±±¾©¿¾Ñ¼", :location => "±±¾©", :spicy => 0, :rate => 3.4, :rate_number =>16)
+Dish.create(:name => "¿¾È«Ñò", :location => "ÐÂ½®", :spicy => 2, :rate => 4.3, :rate_number => 13)
+Dish.create(:name => "³¦·Û", :location => "¹ãÖÝ", :spicy => 0, :rate => 3.2, :rate_number => 17)
+Dish.create(:name => "ºìÉÕÎä²ýÓã", :location => "ºþ±±", :spicy => 3, :rate => 3.9, :rate_number => 9)
